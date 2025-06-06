c4652c029680a2bf45de6d48b7fa041de949fe75
satishdwivedi-01 <dwivedisatish06@gmail.com>
2025-06-06 12:18:23 +0530

    fixed landings background

--- frontend/src/components/crtd Landing page/fresher Jobs/FresherJobs.jsx
+++ frontend/src/components/crtd Landing page/fresher Jobs/FresherJobs.jsx
@@ -3,7 +3,7 @@ import Navbar from '../../Navbar'
 import LaunchYourTech from './LaunchYourTech'
 const fresherJobs = () => {
     return (
-        <div className='bg-white text-black w-[94vw] relative right-[7vw] bottom-[3.7vh] '>
+        <div className='bg-white text-black relative w-[100vw] relative right-[10.7vw]'>
 
       <div className='bg-white sticky top-0 z-50'>
         <Navbar />
